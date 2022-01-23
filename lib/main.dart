c4588c6090991e8a11fb4025bca47665d494d0d5
Alan Sousa da Silva <alanssrv@gmail.com>
2022-01-23 10:04:52 -0300

    grid style & add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,19 @@ class MyApp extends StatelessWidget {
       title: 'DeliMeals',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.pink,
+        ).copyWith(
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline1: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
       home: const CategoriesScreen(),
     );
