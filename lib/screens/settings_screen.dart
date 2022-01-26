dc33ca7d96d460ace2e3da56a4616a03eb0eb8ae
Alan Sousa da Silva <alanssrv@gmail.com>
2022-01-26 13:55:01 -0300

    settings & filter

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,18 +1,94 @@
 import 'package:flutter/material.dart';
+import '../models/settings.dart';
 import '../components/main_drawer.dart';
 
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({Key? key}) : super(key: key);
+class SettingsScreen extends StatefulWidget {
+  final Settings settings;
+  final Function(Settings) onSettingsChanged;
+
+  const SettingsScreen(this.onSettingsChanged, this.settings, {Key? key})
+      : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  late Settings settings;
+
+  @override
+  void initState() {
+    super.initState();
+
+    settings = widget.settings;
+  }
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function(bool) onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: (value) {
+        onChanged(value);
+        widget.onSettingsChanged(settings);
+      },
+      activeColor: Theme.of(context).colorScheme.primary,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Configurações'),
+          title: const Text('Configurações'),
+          centerTitle: true,
         ),
-        drawer: MainDrawer(),
-        body: Center(
-          child: Text('Configurações'),
+        drawer: const MainDrawer(),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                'Configurações',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  _createSwitch(
+                    'Sem Glutén',
+                    'Só exibe refeições sem glutén',
+                    settings.isGlutenFree,
+                    (value) => setState(() => settings.isGlutenFree = value),
+                  ),
+                  _createSwitch(
+                    'Sem Lactose',
+                    'Só exibe refeições sem lactose',
+                    settings.isLactoseFree,
+                    (value) => setState(() => settings.isLactoseFree = value),
+                  ),
+                  _createSwitch(
+                    'Vegana',
+                    'Só exibe refeições vaganas',
+                    settings.isVegan,
+                    (value) => setState(() => settings.isVegan = value),
+                  ),
+                  _createSwitch(
+                    'Vegetariana',
+                    'Só exibe refeições vegetarianas',
+                    settings.isVegetarian,
+                    (value) => setState(() => settings.isVegetarian = value),
+                  )
+                ],
+              ),
+            )
+          ],
         ));
   }
 }
