c9187572209c1568bc076bc81b698791ac131dc0
Alan Sousa da Silva <alanssrv@gmail.com>
2022-01-26 10:20:45 -0300

    tabbar categorias/favoritos

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'screens/tabs_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'utils/app_routes.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -24,16 +25,19 @@ class MyApp extends StatelessWidget {
         canvasColor: const Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-              headline1: const TextStyle(
-                fontSize: 20,
-                fontFamily: 'RobotoCondensed',
-              ),
+            headline1: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
             ),
+            headline6: const TextStyle(
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.w400,
+            )),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => const CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => const TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
-        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => const MealDetailScreen(),
       },
     );
   }
